5fb2a01f539843693470a29efae89d9b04c64bd1
GabrielF4 <112709330+GabrielF4@users.noreply.github.com>
2025-05-12 23:22:49 +0200

    Refactored the code for maintainability and readability

--- pokerhand.py
+++ pokerhand.py
@@ -2,53 +2,14 @@
 from collections import Counter
 from typing import List, Self
 
-class PokerHand:
+class PokerAnalysis:
 
-    RESULT = ["Loss", "Tie", "Win"]
+    RANK_DICT = {
+        "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "T": 10, "J": 11, "Q": 12, "K": 13, "A": 14
+    }
 
-    #Hand rating is based on the texas hold em rules: 1-10 where 1 is the highest rating which is a Royal Flush
-    hand_rating: int = 0
-    #Example of hand: "KH 2H 5H 6H JH"
-    hand: str = ""
-
-    def __init__(self, hand: str):
-        self.hand = hand
-        self.hand_rating = self.rate_hand(hand)
-
-    #Input: Single Card (Example: "KS")
-    # Returns: a integer value based on card value
-    # Return values: 2, 3, 4, 5, 6, 7, 8, 9, 10 (for T), 11 (for J), 12 (for Q), 13 (for K), 14 (for A)
-    def card_value(self, card: str) -> int:
-        if card[0].isdigit():
-            return int(card[0])
-        elif card[0] == "T":
-            return 10
-        elif card[0] == "J":
-            return 11
-        elif card[0] == "Q":
-            return 12
-        elif card[0] == "K":
-            return 13
-        elif card[0] == "A":
-            return 14
-
-    #Input: string (Example: "KS 2H 5C JD TD")
-    # Return: an array based of the card values (Example: [13, 2, 5, 11, 10])
-    def get_card_values(self, hand: str) -> List[int]:
-        arr = hand.split(" ")
-        card_values = []
-
-        for i in range(5):
-            card_values.append(self.card_value(arr[i]))
-
-        return card_values
-
-    #Input: an array with card values (Example: [13, 2, 5, 11, 10])
-    # Return: bool if values are in a sequence
     def is_straight(self, arr: List[int]) -> bool:
-
         arr = sorted(arr)
-        
         for i in range(4):
             #Check for the sequence Ace, 2, 3, 4, 5
             if arr[0] == 2 and arr[1] == 3 and arr[2] == 4 and arr[3] == 5 and arr[4] == 14:
@@ -60,45 +21,50 @@ class PokerHand:
     
     def get_four_alike(self, values: List[int]):
         counts = Counter(values)
-        for key, val in counts.items():
-            if val == 4:
-                return True, key
+        for card_rank, nr_of_cards in counts.items():
+            if nr_of_cards == 4:
+                return True, card_rank
         return False, 0
         
     def get_three_alike(self, values: List[int]):
         counts = Counter(values)
-        for key, val in counts.items():
-            if val == 3:
-                return True, key
+        for card_rank, nr_of_cards in counts.items():
+            if nr_of_cards == 3:
+                return True, card_rank
         return False, 0
         
     def get_pairs(self, values: List[int]):
         pairs = []
         counts = Counter(values)
-        for key, val in counts.items():
-            if val == 2:
-                pairs.append(key)
+        for card_rank, nr_of_cards in counts.items():
+            if nr_of_cards == 2:
+                pairs.append(card_rank)
         return pairs
+    
+    def get_card_values(self, hand: str) -> List[int]:
+        cards = hand.split(" ")
+        return sorted([self.RANK_DICT[x[0]] for x in cards], reverse=True)
+    
+    def get_card_suits(self, hand: str) -> List[str]:
+        cards = hand.split(" ")
+        return [x[1] for x in cards]
 
-
-    #Input: string (Example: "KS 2H 5C JD TD")
-    #Return: integer hand rating based on the texas hold em rules
     def rate_hand(self, hand: str):
 
-        arr = hand.split(" ")
-
         #Get the card values in an array
-        values = sorted([self.card_value(x[0]) for x in arr])
-        suits = [x[1] for x in arr]
+        card_ranks: List[int] = self.get_card_values(hand)
+        suits: List[str] = self.get_card_suits(hand)
 
         #Make a counter for each value
-        counts = Counter(values)
+        counts = Counter(card_ranks)
 
         #Check for similar cards
         four_equal, four_equal_val = self.get_four_alike(counts)
         three_equal, three_equal_val = self.get_three_alike(counts)
 
+        #Check pairs and values of pairs
         pairs = sorted(self.get_pairs(counts), reverse=True)
+        pair, two_pairs = False, False
         if len(pairs) == 2:
             two_pairs = True
             pair_1_val = pairs[0]
@@ -107,15 +73,12 @@ class PokerHand:
             pair = True
             pair_val = pairs[0]
 
-        pair = any(count == 2 for count in counts.values())
-        two_pairs = Counter(counts.values())[2] == 2
-
         #Check for flush and straight
         flush = all(x == suits[0] for x in suits)
-        straight = self.is_straight(values)
+        straight = self.is_straight(card_ranks)
         
         #Royal Flush
-        if flush and straight and values[3] == 13:
+        if flush and straight and card_ranks[1] == 13:
             return 1
         #Straight Flush
         if flush and straight:
@@ -141,27 +104,38 @@ class PokerHand:
         #Pair
         if pair:
             return 9 - pair_val * 0.01
-
         #No special combination of cards
         return 10
-    
-    #Input: PokerHand with the hand of the opponent
-    #Return: "Loss", "Tie" or "Win" depending on who has the better hand 
-    def compare_with(self, other: Self, debug = False) -> str:
 
-        opponent_hand_rating = self.rate_hand(other.hand)
+class PokerHand:
+
+    RESULT = ["Loss", "Tie", "Win"]
+
+    analysis = PokerAnalysis()
+
+    def __init__(self, hand: str):
+        self.hand = hand
+    
+    def compare_with(self, other: Self) -> str:
+
+        debug = True
+
+        your_hand_rating = self.analysis.rate_hand(self.hand)
+        opponent_hand_rating = self.analysis.rate_hand(other.hand)
+
+        if debug:
+            print(f"Your hand: {self.hand} - Rating = {your_hand_rating}")
+            print(f"Opponents Hand: {other.hand} - Rating = {opponent_hand_rating}")
 
         #Comparing card ratings based on the texas hold em rules
-        if debug:
-            print(f"Your card rating: {self.hand_rating}\nOpponent hand rating: {opponent_hand_rating}")
-        if self.hand_rating < opponent_hand_rating:
+        if your_hand_rating < opponent_hand_rating:
             return self.RESULT[2]
-        elif self.hand_rating > opponent_hand_rating:
+        elif your_hand_rating > opponent_hand_rating:
             return self.RESULT[0]
         else:
             #Get a value for each card and put it into an array
-            your_card_values: List[int] = sorted(self.get_card_values(self.hand), reverse=True)
-            opponent_card_values: List[int] = sorted(self.get_card_values(other.hand) , reverse=True)
+            your_card_values: List[int] = self.analysis.get_card_values(self.hand)
+            opponent_card_values: List[int] = self.analysis.get_card_values(other.hand)
 
             if debug:
                 print(f"Your card values: {your_card_values}\nOpponent hand values: {opponent_card_values}")
@@ -177,9 +151,16 @@ class PokerHand:
 
 if __name__ == "__main__":
     
-    myhand = PokerHand("4S 6S QS 4H 5H")
+    myhand = PokerHand("KS TS QS JS AS")
     opponent_hand = PokerHand("TS 2D TH 7C 9S")
 
-    result = myhand.compare_with(opponent_hand, debug=True)
+    result = myhand.compare_with(opponent_hand)
     
-    print(result)
+    if(result == "Win"):
+        print("Your hand wins!")
+    elif(result == "Loss"):
+        print("Your hand loses!")
+    else:
+        print("It's a draw!")
+
+    
